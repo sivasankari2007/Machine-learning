766c25375a3d6db1e22fa0b7339c632c0bba636a
sivasankari2007 <sivasankarisabarees@gmail.com>
2025-12-15 20:32:39 +0530

    Update app.py

--- water/app.py
+++ water/app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # Load model and scaler
 # -------------------------------
 try:
-    with open("", "rb") as f:
+    with open("water/hardness_model.pkl", "rb") as f:
         model, scaler = pickle.load(f)
 except FileNotFoundError:
     st.error("❌ hardness_model.pkl not found. Please place it in the same folder as app.py")
@@ -32,3 +32,4 @@ st.write("Enter water quality parameters to predict if it is potable (safe to dr
 # -------------------------------
 ph = st.number_input("pH value", min_value=0.0, max_value=14.0, value=7.0, step=0.1)
 Hardness = st.number_input("Hardness (mg/L)", min_value=0.0, max_value=500.0, value=150.0, step=1.0)
+
