4a77f18c355669f868404a8eb39de688413d3942
sivasankari2007 <sivasankarisabarees@gmail.com>
2025-12-21 18:41:01 +0530

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -32,7 +32,7 @@ def load_text(filename):
         return [line.strip() for line in f if line.strip()]
 
 responses = {
-    "joke": load_text("joke.txt"),
+    "joke": load_text("chatbot/data-20251221T130125Z-1-001/data/joke.txt"),
     "motivation": load_text("motivation.txt"),
     "study": load_text("studytips.txt"),
     "greeting": load_text("greetings.txt"),
