5df5b5c93d8212b625a90063dbd73b816851bd3b
sivasankari2007 <sivasankarisabarees@gmail.com>
2025-12-21 18:48:47 +0530

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -3,8 +3,8 @@ import pickle
 import random
 import numpy as np
 import requests
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 
 # -----------------------------
 # PATHS
