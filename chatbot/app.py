8e9248b713cb0decb25eac023618e812fa13e6f7
sivasankari2007 <sivasankarisabarees@gmail.com>
2025-12-23 10:35:04 +0530

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -34,7 +34,7 @@ def load_text(filename):
 data_files = {
     "joke": load_text("chatbot/data/joke.txt"),
     "motivation": load_text("chatbot/data/motivation.txt"),
-    "study": load_text("studytips.txt"),
+    "study": load_text("chatbot/data/studytips.txt"),
     "greeting": load_text("chatbot/data/greetings.txt"),
     "text": load_text("text.txt")
 }
